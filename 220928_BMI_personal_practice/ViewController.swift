255b91cdab78e84aa0dd63ee173da11117c5b9e3
Yi <66saenggak99@gmail.com>
2022-09-28 17:35:47 +0900

    Initial Commit

---
+++ 220928_BMI_personal_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  220928_BMI_personal_practice
+//
+//  Created by Yi_Mac on 2022/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
